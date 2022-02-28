972048d0554927651b6bd922529a725fca835b03
douruanliang <douruanliang@meishubao.com>
2022-02-28 10:43:34 +0800

    调整目录结构

--- app/src/main/java/io/dourl/mqtt/MainActivity.java
+++ app/src/main/java/io/dourl/mqtt/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.dourl.mqtt.manager.MqttManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
