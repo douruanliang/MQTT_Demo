96d61138053d35d1d93382a3ca0a62966a8731f0
douruanliang <douruanliang@meishubao.com>
2022-03-18 16:37:58 +0800

    不想改了-fuck

---
+++ mq/src/main/java/io/dourl/mqtt/ui/adpater/chat/ChatAdapter.java
@@ -0,0 +1,198 @@
+package io.dourl.mqtt.ui.adpater.chat;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import io.dourl.mqtt.base.log.LoggerUtil;
+import io.dourl.mqtt.bean.MessageModel;
+import io.dourl.mqtt.model.BaseUser;
+import io.dourl.mqtt.model.customenum.ManagerType;
+import io.dourl.mqtt.model.message.chat.AudioBody;
+import io.dourl.mqtt.model.message.chat.BaseMsgBody;
+import io.dourl.mqtt.model.message.chat.BodyType;
+import io.dourl.mqtt.model.message.chat.ImageBody;
+import me.drakeet.multitype.MultiTypeAdapter;
+
+/**
+ * Created by SpiritTalk on 17/1/13.
+ */
+
+public class ChatAdapter extends MultiTypeAdapter {
+    /**
+     * 消息间隔时间为 60 秒
+     */
+    private static final int INTERVAL_TIME = 60 * 1000;
+    private List<MessageModel> mDataList;
+
+    public OnItemActionListener mOnItemActionListener;
+    public HashMap<String, BaseUser> mClanMember = new HashMap<>();
+    public ManagerType mMyManagrType = ManagerType.normal;
+
+    public ChatAdapter(List<MessageModel> list) {
+        super(list);
+        mDataList = list;
+    }
+
+    /**
+     * 判断当前消息是否需要显示时间
+     *
+     * @param position
+     */
+    private void checkIsShowTime(int position) {
+        // 超出长度，直接返回
+        if (position >= mDataList.size()) return;
+
+        MessageModel curMessage = mDataList.get(position);
+        // 最后一个，必须显示
+        if (position == mDataList.size() - 1) {
+            curMessage.setShowTime(true);
+            return;
+        } else {
+            curMessage.setShowTime(false);
+        }
+
+        MessageModel preMessage = mDataList.get(position + 1);
+        if (Math.abs(curMessage.getTime() - preMessage.getTime()) > INTERVAL_TIME) {
+            curMessage.setShowTime(true);
+        } else {
+            curMessage.setShowTime(false);
+        }
+
+    }
+
+    public void setClanMember(HashMap<String, BaseUser> clanMember) {
+        this.mClanMember.clear();
+        this.mClanMember = clanMember;
+    }
+
+    public void setMyManagrType(ManagerType myManagrType) {
+        mMyManagrType = myManagrType;
+    }
+
+
+    public void setData(List<MessageModel> messages) {
+        if (messages != null && !messages.isEmpty()) {
+            mDataList.clear();
+            mDataList.addAll(messages);
+            notifyDataSetChanged();
+        }
+    }
+
+    public void addPreviousPage(List<MessageModel> messages) {
+        if (messages != null && !messages.isEmpty()) {
+            int positionStart = mDataList.size();
+            mDataList.addAll(messages);
+            notifyItemRangeInserted(positionStart, messages.size());
+        }
+    }
+
+    public void addData(MessageModel message) {
+        mDataList.add(0, message);
+        notifyItemInserted(0);
+    }
+
+    public void removeData(MessageModel message) {
+        int index = mDataList.indexOf(message);
+        mDataList.remove(message);
+        notifyItemRemoved(index);
+    }
+
+    public void updateData(MessageModel message) {
+        for (int i = mDataList.size() - 1; i >= 0; i--) {
+            MessageModel m = mDataList.get(i);
+            if (m.equals(message)) {
+                m.setDownloading(message.isDownloading());
+                m.setSendStatus(message.getSendStatus());
+                if (m.getBodyType() == BodyType.TYPE_AUDIO) {
+                    ((AudioBody) m.getBody()).setPlaying(((AudioBody) message.getBody()).isPlaying());
+                } else if (m.getBodyType() == BodyType.TYPE_RED_PACKET) {
+                    m.setBody(message.getBody());
+                }
+                notifyItemChanged(i);
+                LoggerUtil.d("update msg at: " + i);
+                return;
+            }
+        }
+    }
+
+    public void setOnItemActionListener(OnItemActionListener onItemActionListener) {
+        mOnItemActionListener = onItemActionListener;
+    }
+
+
+    public List<MessageModel> getDataList() {
+        return mDataList;
+    }
+
+    /**
+     * 获取当前会话的图片
+     *
+     * @return
+     */
+    public ArrayList<String> getImageDataList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = mDataList.size() - 1; i >= 0; i--) {
+            MessageModel model = mDataList.get(i);
+            if (model.getBody() instanceof ImageBody) {
+                String local = model.getLocalPath();
+               /* if (FileUtils.isFileExist(local)) {
+                    list.add(local);
+                } else {
+                    list.add(model.getBody().getServerPath());
+                }*/
+            }
+        }
+        return list;
+    }
+
+    public OnItemActionListener getOnItemActionListener() {
+        return mOnItemActionListener;
+    }
+
+    public interface OnItemActionListener {
+
+        /**
+         * 复制
+         *
+         * @param message
+         */
+        void copy(MessageModel message);
+
+        /**
+         * 复制
+         *
+         * @param message
+         */
+        void save(MessageModel message);
+
+        /**
+         * 删除
+         *
+         * @param message
+         */
+        void delete(MessageModel message);
+
+        /**
+         * 转发
+         *
+         * @param message
+         */
+        void forward(MessageModel message);
+
+        /**
+         * 撤回
+         */
+        void withdraw(MessageModel message);
+
+        /**
+         * @
+         */
+        void at(MessageModel message);
+
+    }
+
+}
---
+++ mq/src/main/java/io/dourl/mqtt/ui/adpater/chat/ChatTextProvider.java
@@ -0,0 +1,101 @@
+package io.dourl.mqtt.ui.adpater.chat;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
+import io.dourl.mqtt.R;
+import io.dourl.mqtt.model.message.chat.TextBody;
+import io.dourl.mqtt.utils.chat.TextBodyContentUtils;
+
+
+/**
+ * Created by SpiritTalk on 17/1/13.
+ */
+
+public class ChatTextProvider extends ChatFrameProvider<TextBody, ChatTextProvider.ViewHolder> {
+
+    @Override
+    RecyclerView.ViewHolder onCreateContentViewHolder(LayoutInflater inflater, ViewGroup parent) {
+        View view = inflater.inflate(R.layout.item_chat_text, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    void onBindContentViewHolder(ViewHolder holder, TextBody textBody, boolean isMine) {
+        holder.bindData(textBody, isMine);
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private Context mContext;
+        private TextView mTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mContext = itemView.getContext();
+            mTextView = (TextView) itemView.findViewById(R.id.textView);
+        }
+
+        public void bindData(TextBody textBody, boolean isMine) {
+            if (textBody.getSpanContent() == null) {
+                final StringBuilder builder = new StringBuilder();
+                // 设置内容
+                Spannable span = null;
+//                for (TextBody.TextEntity textEntity : textBody.getContent()) {
+//                    if (textEntity.getT().equals("notice")) {
+//                        List<TextBody.TextEntity> notice = new Gson().fromJson(textEntity.getC(), new TypeToken<List<TextBody.TextEntity>>() {
+//                        }.getType());
+//                        String title = mContext.getResources().getString(R.string.chat_all);
+//                        builder.append(title);
+//                        builder.append("\n");
+//                        for (TextBody.TextEntity item : notice) {
+//                            builder.append(item.getC());
+//                        }
+//                        span = Spannable.Factory.getInstance().newSpannable(builder.toString());
+//                        span.setSpan(new ForegroundColorSpan(Color.BLUE), builder.indexOf(title), builder.indexOf(title) + title.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+//                    } else {
+//                        builder.append(textEntity.getC());
+//                        // 设置内容
+//                        span = ImSmileUtils.getSmiledText(mContext, builder.toString());
+//                    }
+//                }
+                span = TextBodyContentUtils.getSpannableContent(mContext, textBody.getContent());
+                mTextView.setText(span, TextView.BufferType.SPANNABLE);
+            } else {
+                mTextView.setText(textBody.getSpanContent(), TextView.BufferType.SPANNABLE);
+            }
+
+            mTextView.setMaxWidth(textWidth);
+            RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) mTextView.getLayoutParams();
+            if (isMine) {
+                //mTextView.setBackgroundResource(R.drawable.round_corner_rect_green);
+                mTextView.setTextColor(mContext.getResources().getColor(R.color.white));
+                lp.leftMargin = 0;
+                lp.rightMargin = imageMargin;
+                mTextView.setPadding(textPadding, textPadding, textPadding, textPadding);
+            } else {
+               // mTextView.setBackgroundResource(R.drawable.round_corner_rect_white);
+                mTextView.setTextColor(mContext.getResources().getColor(R.color.color_5d697f));
+                lp.leftMargin = imageMargin;
+                lp.rightMargin = 0;
+                mTextView.setPadding(textPadding, textPadding, textPadding, textPadding);
+            }
+            mTextView.setLayoutParams(lp);
+        }
+    }
+}
